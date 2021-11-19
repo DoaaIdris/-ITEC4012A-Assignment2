6a3f16e4e5494313af8de8f996412d57b7b7c1de
Doaa <doaa.id00@gmail.com>
2021-11-18 23:50:00 -0500

    updated post component and added state for liking a post

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { Stories } from "../../Stories";
-import { Post } from "../../Posts";
+import { Post } from "../../Post";
 
 export const HomePage = () => {
 
@@ -9,7 +9,8 @@ export const HomePage = () => {
         <div className="mainPosts">
           <Stories></Stories>
           <Post username="tasia.m.s" profilePhoto="https://pbs.twimg.com/media/EbUf5RhWAAIIkYA.jpg"
-          photo="https://i.pinimg.com/originals/f6/f0/91/f6f09138a5dce9e5ca1c28e066e8f7d1.jpg"></Post>
+          photo="https://i.pinimg.com/originals/f6/f0/91/f6f09138a5dce9e5ca1c28e066e8f7d1.jpg"
+          likes={500} caption="mandy from totally spies" time="4 days ago"></Post>
         </div>   
       </div>
     
