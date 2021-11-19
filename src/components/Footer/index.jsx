fdecb87710f8a5a730f160809c68ab6478ec9247
Doaa <doaa.id00@gmail.com>
2021-11-19 14:56:19 -0500

    added footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,56 @@
+import "./styles.css";
+
+export const Footer = () => {
+ 
+  return (
+     <footer className="footer">
+         <ul className="first-row">
+            <li className="first-row-item">
+                <p>Meta</p>
+            </li>
+            <li className="first-row-item">
+                <p>About</p>
+            </li>
+            <li className="first-row-item">
+                <p>Blog</p>
+            </li>
+            <li className="first-row-item">
+                <p>Jobs</p>
+            </li>
+            <li className="first-row-item">
+                <p>Help</p>
+            </li>
+            <li className="first-row-item">
+                <p>API</p>
+            </li>
+            <li className="first-row-item">
+                <p>Privacy</p>
+            </li>
+            <li className="first-row-item">
+                <p>Terms</p>
+            </li>
+            <li className="first-row-item">
+                <p>Top Accounts</p>
+            </li>
+            <li className="first-row-item">
+                <p>Hashtags</p>
+            </li>
+            <li className="first-row-item">
+                <p>Locations</p>
+            </li>
+            <li className="first-row-item">
+                <p>Instagram Lite</p>
+            </li>
+         </ul>
+         <ul className="second-row">
+         <li className="first-row-item">
+                <p>English</p>
+            </li>
+            <li className="first-row-item">
+                <p>© 2021 Instagram from Meta</p>
+            </li>
+         </ul>
+     </footer>
+   
+  );
+};
