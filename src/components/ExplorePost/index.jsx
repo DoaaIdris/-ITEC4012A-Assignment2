ed5a76e7f6610114ee329edff4118ad2b77fb361
Doaa <doaa.id00@gmail.com>
2021-11-19 19:29:24 -0500

    added overlay state for explore page posts

--- src/components/ExplorePost/index.jsx
+++ src/components/ExplorePost/index.jsx
@@ -1,15 +1,33 @@
 import "./styles.css";
 import PropTypes from 'prop-types';
+import {AiFillHeart} from 'react-icons/ai'
+import {FaComment} from 'react-icons/fa'
+import { useState } from "react";
 
 export const ExplorePost = (props) => {
- 
+
 const {likes, comments, photo} = props;
 
+const [isOverlay, setIsOverlay]  = useState(false);
+
   return (
-     <div className="explore-post">
-         <img src={photo}/>
-     </div>
-   
+    <div className="explore-post" 
+        onMouseEnter={() => setIsOverlay(true)}
+        onMouseLeave={() => setIsOverlay(false)}>
+    
+        <img src={photo}/>
+        {isOverlay && (
+          <div className="explore-post-overlay"> 
+          <p className="explore-post-likes">
+            <AiFillHeart className="icon"/>{likes}
+          </p>
+          <p className="explore-post-comments">
+          <FaComment className="icon"/>{comments}
+          </p>
+        </div>
+        )}   
+    </div>
+
   );
 };
 
